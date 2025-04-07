733582db159c839a314508f16563a9677d48caaf
developer123 <lo14ubna02@gmail.com>
2025-04-07 14:03:40 +0100

    changes on files

--- Projet/app1/urls.py
+++ Projet/app1/urls.py
@@ -11,7 +11,7 @@ path('accueil/',views.home, name='home'),
 
 path('', views.restaurant_list, name='restaurant_list'),
 path('add/', views.restaurant_create, name='restaurant_create'),
-path('<int:pk>/edit/', views.restaurant_update, name='restaurant_update'),
+path('<int:pk>/edit/', views.RestaurantUpdateView.as_view(), name='restaurant_update'),
 path('<int:pk>/delete/', views.restaurant_delete, name='restaurant_delete'),
 
 ]
--- Projet/app1/views.py
+++ Projet/app1/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Restaurant
+from .forms import RestaurantForm
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
 
 def home(request):
@@ -11,5 +14,30 @@ def home(request):
 
 def restaurant_list(request):
     restaurants = Restaurant.objects.all()
-    return render(request,'restaurants/index.html')
+    return render(request,'restaurants/index.html', {'restaurants':restaurants})
 
+
+def restaurant_create(request):
+    form = RestaurantForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('restaurant_list')
+    return render(request, 'restaurants/form.html', {'form': form})
+
+
+class RestaurantUpdateView(UpdateView):
+    model = Restaurant
+    form_class = RestaurantForm
+    template_name = 'restaurants/form_update.html'
+    context_object_name = 'restaurant'
+
+    def form_valid(self,form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('restaurant_list')
+
+
+
+def restaurant_delete(request):
+    pass
