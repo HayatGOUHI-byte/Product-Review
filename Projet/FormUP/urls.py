7a6d28f0dcad3638acc93e0aa9218091fe570441
developer123 <lo14ubna02@gmail.com>
2025-04-10 17:20:56 +0100

    Adding an other application

--- Projet/FormUP/urls.py
+++ Projet/FormUP/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 path('',views.index_formation, name='index_formation'),
 path('all_formations/',views.all_formations, name='all_formations'),
+path('tous_cours/', views.tous_cours, name='tous_cours.html'),
 
 ]
--- Projet/FormUP/views.py
+++ Projet/FormUP/views.py
@@ -14,3 +14,7 @@ def index_formation(request):
 def all_formations(request):
 	all_formations = Formation.objects.all()
 	return render(request, 'all_formations.html', {'all_formations':all_formations})
+
+def tous_cours(request):
+	cours = Cours.objects.all()
+	return render(request,'tous_cours.html', {'cours': cours})
